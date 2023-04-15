e2bdcaf78e5c15f96af353c03b5cad16f88ec339
lijianlong <lijianlong1314@outlook.com>
2023-04-15 12:50:57 +0800

    分支测试1

--- demo/src/main/java/com/lijian/DemoApplication.java
+++ demo/src/main/java/com/lijian/DemoApplication.java
@@ -1,12 +1,14 @@
 package com.lijian;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
+        log.info("哈哈");
         SpringApplication.run(DemoApplication.class, args);
     }
 
